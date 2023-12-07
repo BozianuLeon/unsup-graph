f67974ba7de08542490535ae72e6beed7c6c0217
Leon Bozianu <74475015+BozianuLeon@users.noreply.github.com>
2023-12-07 19:55:27 +0100

    important utils from SSD

--- calo/data/get_cluster_point_clouds.py
+++ calo/data/get_cluster_point_clouds.py
@@ -1,15 +1,19 @@
 import numpy as np
 import scipy
+import h5py
 import matplotlib.pyplot as plt
+
 
 import torch
 import torch_geometric
 
 
+# TODO: NEED WRAP CHECKS
+# TODO: NEED TO GET CELLS FROM BOXES
+# TODO: NEED TO FIND THE CLUSTERS THAT ARE INSIDE EACH TRUTH BOX
 
-
-with open("/Users/leonbozianu/work/phd/graph/dmon/struc_array.npy", "rb") as f:
-    inference = np.load(f)
+with open("/Users/leonbozianu/work/phd/graph/dmon/struc_array.npy", "rb") as file:
+    inference = np.load(file)
 
 
 for i in range(1):
@@ -24,7 +28,43 @@ for i in range(1):
     pees = torch.tensor(pees)
     tees = torch.tensor(tees)
 
+    #make boxes cover extent
+    tees[:,(0,2)] = (tees[:,(0,2)]*(extent_i[1]-extent_i[0]))+extent_i[0]
+    tees[:,(1,3)] = (tees[:,(1,3)]*(extent_i[3]-extent_i[2]))+extent_i[2]
 
+    pees[:,(0,2)] = (pees[:,(0,2)]*(extent_i[1]-extent_i[0]))+extent_i[0]
+    pees[:,(1,3)] = (pees[:,(1,3)]*(extent_i[3]-extent_i[2]))+extent_i[2]
+
+    #wrap check
+
+
+    #get the cells
+    h5f = inference[i]['h5file']
+    event_no = inference[i]['event_no']
+
+    file = "/Users/leonbozianu/work/phd/graph/dmon/user.cantel.34126190._0000{}.calocellD3PD_mc16_JZ4W.r10788.h5".format(h5f.decode('utf-8'))
+    with h5py.File(file,"r") as f:
+        h5group = f["caloCells"]
+        cells = h5group["2d"][event_no]
+
+    clusters_file = "/Users/leonbozianu/work/phd/graph/dmon/user.cantel.34126190._0000{}.topoclusterD3PD_mc16_JZ4W.r10788.h5".format(h5f.decode('utf-8'))
+    with h5py.File(clusters_file,"r") as f:
+        cl_data = f["caloCells"] 
+        event_data = cl_data["1d"][event_no]
+        cluster_data = cl_data["2d"][event_no]
+        cluster_cell_data = cl_data["3d"][event_no]
+
+
+
+
+
+
+#now we have the boxes, the clusters and the cells we can begin making
+
+
+#file structure is
+#plots/index/total_event/
+#plots/index/cluster_X/
 
 
 
---
+++ utils.py
@@ -0,0 +1,139 @@
+import numpy as np
+import torch
+import torchvision
+
+
+
+def wrap_check_truth(boxes,ymin,ymax):
+    #here we look at truth boxes, remove the (wrapped) "duplicates" 
+    #and mitigate those crossing the discontinuity
+    #input is a np.ndarray containing the boxes in xyxy coords, after multiplication of extent
+    if isinstance(boxes,np.ndarray):
+        boxes = torch.tensor(boxes)
+    suppress = np.zeros(len(boxes))
+    for j in range(len(boxes)):
+        box_j = boxes[j]
+
+        #case (A) the truth box lies entirely outside the true phi range
+        if (box_j[1]>ymax) or (box_j[3]<ymin):
+            suppress[j] = 1
+        
+        #case (B) the truth box has two corners outside the true phi range
+        #check the IoU of the truth box with its duplicate, remove just one of these
+        elif (box_j[1] < ymin) or (box_j[3] > ymax):
+            modded_box_j = box_j + (-1*torch.sign(box_j[1])) * torch.tensor([0.0, 2*np.pi, 0.0, 2*np.pi])
+            overlaps = torchvision.ops.box_iou(modded_box_j.unsqueeze(0), boxes)
+            wrapped_box = boxes[torch.argmax(overlaps)]
+
+            #keep the truth box with the largest area (can be different due to merging).
+            suppress[j] = max(suppress[j],(box_j[2]-box_j[0])*(box_j[3]-box_j[1])<(wrapped_box[2]-wrapped_box[0])*(wrapped_box[3]-wrapped_box[1]))
+
+    boxes = boxes.numpy()
+    return boxes[np.where(suppress==0)]
+
+
+
+
+def RetrieveCellIdsFromBox(cells,boxes):
+
+    ymin,ymax = min(cells['cell_phi']),max(cells['cell_phi']) # get physical bounds of calo cells
+    list_containing_all_cells = []
+    for box in boxes:
+        eta_min,phi_min,eta_max,phi_max = box
+        x_condition = np.logical_and.reduce((cells['cell_eta']>=eta_min, cells['cell_eta']<=eta_max))
+        
+        #box straddles bottom of image
+        if (phi_min < ymin) and (phi_max > ymin):
+            modded_box = box + np.array([0.0, 2*np.pi, 0.0, 2*np.pi])
+            top_of_top_box = min(modded_box[3],ymax)
+            y_condtion1 = np.logical_and.reduce((cells['cell_phi']>=ymin, cells['cell_phi']<=phi_max))
+            y_condtion2 = np.logical_and.reduce((cells['cell_phi']>=modded_box[1], cells['cell_phi']<=top_of_top_box))
+            y_cond = np.logical_or(y_condtion1,y_condtion2)
+        
+        #box straddles top of image
+        elif (phi_max > ymax) and (phi_min < ymax):
+            modded_box = box - np.array([0.0, 2*np.pi, 0.0, 2*np.pi])
+            bottom_of_bottom_box = min(modded_box[1],ymin)
+            y_condtion1 = np.logical_and.reduce((cells['cell_phi'] >= phi_min, cells['cell_phi'] <= ymax))
+            y_condtion2 = np.logical_and.reduce((cells['cell_phi'] >= bottom_of_bottom_box, cells['cell_phi'] <= modded_box[3]))
+            y_cond = np.logical_or(y_condtion1,y_condtion2)
+
+        #box is completely above top
+        elif (phi_max < ymin):
+            modded_box = box + np.array([0.0, 2*np.pi, 0.0, 2*np.pi])
+            y_cond = np.logical_and.reduce((cells['cell_phi']>=modded_box[1], cells['cell_phi']>=modded_box[3]))
+
+        elif (phi_min > ymax):
+            modded_box = box - np.array([0.0, 2*np.pi, 0.0, 2*np.pi])
+            y_cond = np.logical_and.reduce((cells['cell_phi']>=modded_box[1], cells['cell_phi']>=modded_box[3]))
+        else:
+            y_cond = np.logical_and.reduce((cells['cell_phi']>=phi_min, cells['cell_phi']<=phi_max)) #multiple conditions #could use np.all(x,axis)
+        
+        tot_cond = np.logical_and(x_condition,y_cond)
+        cells_here = cells[np.where(tot_cond)][['cell_E','cell_eta','cell_phi','cell_Sigma','cell_IdCells','cell_xCells','cell_yCells','cell_zCells']]
+        if len(cells_here):
+            list_containing_all_cells.append(cells_here)
+        else:
+            # so that we know where there were no cells!
+            # placeholder_values = np.array([(-1.0,-99.9,-99.9,-1.0,-10.0)],
+            #     dtype=[('cell_E', '<f4'), ('cell_eta', '<f4'), ('cell_phi', '<f4'), ('cell_Sigma', '<f4'), ('cell_IdCells', '<u4')])
+            # list_containing_all_cells.append(placeholder_values)
+            list_containing_all_cells.append(None)
+
+    # Check that the list does not include None/placeholder values and if it does, remove it
+    filtered_list = [x for x in list_containing_all_cells if x is not None]
+    # somelist = [x for x in list_containing_all_cells if not min(x['cell_phi']) < -99.0]
+
+    return filtered_list
+
+
+def RetrieveCellIdsFromCluster(cells,cluster_cell_info):
+    # Inputs
+    # cluster_cell_info, structured array containing cluster *cell* information
+    #                   including cell ids for cells in topocluster
+    # cells, structured array containing all cells in event ['cell_*'] properties
+    # Outputs
+    # cell_ids, array containing cell ids of all cells inside this topocluster
+
+    list_containing_all_cells = []
+    for cluster in range(len(cluster_cell_info)):
+        cell_mask = np.isin(cells['cell_IdCells'],cluster_cell_info[cluster]['cl_cell_IdCells'])
+        desired_cells = cells[cell_mask][['cell_E','cell_eta','cell_phi','cell_Sigma','cell_IdCells','cell_xCells','cell_yCells','cell_zCells']]
+        list_containing_all_cells.append(desired_cells)
+    # return desired_cells
+    return list_containing_all_cells
+
+
+
+
+#for finding which clusters are in each box
+def total_energy_in_truth_box(cluster_d, cluster_cell_d, cells_this_event, boxes):
+    # number of clusters inside a box based on the cells in that cluster
+    # can overestimate due to wrapping. Sometimes boxes that go "off the page" 
+    # count clusters that are at the other side of the image, this is okay!
+
+    list_cl_cell_E = []
+    list_cl_E = []
+    l_topo_cells = RetrieveCellIdsFromCluster(cells_this_event,cluster_cell_d)
+    for truth_box in boxes:
+        cluster_E_in_tbox = 0
+        cluster_cell_E_in_tbox = 0
+        for cl_no in range(len(l_topo_cells)):
+            cluster_cells = l_topo_cells[cl_no]
+            #if x condition satisfied
+            if truth_box[0] <= np.mean(cluster_cells['cell_eta']) <= truth_box[2]:
+                #if y condition satisfied
+                y_mean_circ = circular_mean(cluster_cells['cell_phi'])
+                if truth_box[1] <= y_mean_circ <= truth_box[3]:
+                    cluster_E_in_tbox += cluster_d[cl_no]['cl_E_em']
+                    cluster_E_in_tbox += cluster_d[cl_no]['cl_E_had']
+                    cluster_cell_E_in_tbox += sum(cluster_cells['cell_E'])
+                elif truth_box[1] <= (y_mean_circ + (-1*np.sign(y_mean_circ))*2*np.pi) <= truth_box[3]:
+                    cluster_E_in_tbox += cluster_d[cl_no]['cl_E_em']
+                    cluster_E_in_tbox += cluster_d[cl_no]['cl_E_had']
+                    cluster_cell_E_in_tbox += sum(cluster_cells['cell_E'])
+        
+        list_cl_cell_E.append(cluster_cell_E_in_tbox)
+        list_cl_E.append(cluster_E_in_tbox)
+
+    return  list_cl_E, list_cl_cell_E
