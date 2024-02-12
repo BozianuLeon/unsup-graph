2d1e2bc14f309f130ad56f381ef350f2b7492048
Leon Bozianu <74475015+BozianuLeon@users.noreply.github.com>
2024-02-12 13:44:14 +0100

    bdt + wandb

--- real/bdt_n_clusters.py
+++ real/bdt_n_clusters.py
@@ -1,13 +1,16 @@
 import torch
 import torchvision
 import torch_geometric
-import h5py
-import os
 import numpy as np
 import numpy.lib.recfunctions as rf
-import pickle
+import matplotlib.pyplot as plt
+import matplotlib
+import h5py
+import os
+import pandas as pd
+# import xgboost
 
-from utils import wrap_check_truth, perpendicular_dists, RetrieveClusterCellsFromBox
+from utils import wrap_check_truth, perpendicular_dists, RetrieveCellIdsFromCluster, RetrieveClusterCellsFromBox
 
 MIN_CELLS_PHI,MAX_CELLS_PHI = -3.1334076, 3.134037
 MIN_CELLS_ETA,MAX_CELLS_ETA = -4.823496, 4.823496
@@ -15,20 +18,15 @@ MIN_CELLS_ETA,MAX_CELLS_ETA = -4.823496, 4.823496
 
 
 
-
-
-def make_calo_data_list(
+def make_bdt_training(
         inference_array,
         box_eta_cut=1.5,
-        cell_significance_cut=2,
-        k=3,    
     ):
     '''
     Turn truth (green) boxes into point clouds in pytorch geometric, save these for later training
     '''
 
-    data_list = list()
-    cluster_list = list()
+    n_clusters_per_box, n_cells_per_box, n_cells2sig_per_box, n_cells4sig_per_box, box_significance, n_cells15sig_per_box, n_cells1sig_per_box, box_etas, box_areas = list(), list(), list(), list(), list(), list(), list(), list(), list()
     for i in range(len(inference_array)):
         h5f = inference_array[i]['h5file']
         event_no = inference_array[i]['event_no']
@@ -48,6 +46,7 @@ def make_calo_data_list(
 
             pees[:,(0,2)] = (pees[:,(0,2)]*(extent_i[1]-extent_i[0]))+extent_i[0]
             pees[:,(1,3)] = (pees[:,(1,3)]*(extent_i[3]-extent_i[2]))+extent_i[2]
+
 
             #wrap check
             # pees = wrap_check_NMS(pees,scores,MIN_CELLS_PHI,MAX_CELLS_PHI,threshold=0.2)
@@ -72,32 +71,33 @@ def make_calo_data_list(
                 truth_box_cells_i = list_truth_cells[truth_box_number]
                 cluster_cells_i = list_cl_cells[truth_box_number]
 
+                # Take boxes in central region
                 if (np.abs(np.mean(truth_box_cells_i['cell_eta']))<box_eta_cut):
-                    # only take cells above 2 sigma
-                    mask = abs(truth_box_cells_i['cell_E'] / truth_box_cells_i['cell_Sigma']) >= cell_significance_cut
+                    mask = abs(truth_box_cells_i['cell_E'] / truth_box_cells_i['cell_Sigma']) >= 2
                     truth_box_cells_2sig_i = truth_box_cells_i[mask]
+                    # Take boxes with >50 2sigma cells
                     if (len(truth_box_cells_2sig_i['cell_eta'])>50):
-                        # calculate edges
-                        struc_array = truth_box_cells_2sig_i[['cell_xCells','cell_yCells','cell_zCells','cell_E','cell_eta','cell_phi','cell_Sigma','cell_IdCells','cell_TimeCells']].copy()
-                        feature_matrix =  rf.structured_to_unstructured(struc_array,dtype=np.float32)
-                        # calculate features
-                        coordinates = torch.tensor(feature_matrix[:,[0,1,2]])
-                        cell_radius = torch.tensor(np.sqrt(truth_box_cells_2sig_i['cell_yCells']**2+truth_box_cells_2sig_i['cell_xCells']**2))
-                        cell_significance = torch.tensor(abs(truth_box_cells_2sig_i['cell_E'] / truth_box_cells_2sig_i['cell_Sigma']))
-                        cell_ids = torch.tensor(truth_box_cells_2sig_i['cell_IdCells'].astype(np.int64))
+                        n_clusters_per_box.append(len(cluster_cells_i))
+                        n_cells_per_box.append(len(truth_box_cells_i))
+                        n_cells2sig_per_box.append(len(truth_box_cells_i[abs(truth_box_cells_i['cell_E'] / truth_box_cells_i['cell_Sigma']) >= 2]))
+                        n_cells4sig_per_box.append(len(truth_box_cells_i[abs(truth_box_cells_i['cell_E'] / truth_box_cells_i['cell_Sigma']) >= 4]))
+                        n_cells15sig_per_box.append(len(truth_box_cells_i[abs(truth_box_cells_i['cell_E'] / truth_box_cells_i['cell_Sigma']) >= 1.5]))
+                        n_cells1sig_per_box.append(len(truth_box_cells_i[abs(truth_box_cells_i['cell_E'] / truth_box_cells_i['cell_Sigma']) >= 1]))
 
-                        weighted_mean = torch.sum(coordinates*cell_significance.view(-1,1), dim=0) / torch.sum(cell_significance)
-                        perp_vectors = perpendicular_dists(coordinates,weighted_mean)
-                        cell_delta_R = torch.linalg.norm(perp_vectors,dim=1)
-                        # feature_tensor = torch.column_stack((torch.tensor(feature_matrix),cell_radius, cell_delta_R,cell_significance))
+                        box_etas.append(np.dot(truth_box_cells_i['cell_eta'],np.abs(truth_box_cells_i['cell_E'])) / sum(np.abs(truth_box_cells_i['cell_E'])))
+                        box_significance.append(sum(truth_box_cells_i['cell_E'] / np.sqrt(sum(truth_box_cells_i['cell_Sigma']**2))))
+                        box_areas.append((tees[truth_box_number][2]-tees[truth_box_number][0])*(tees[truth_box_number][3]-tees[truth_box_number][1]))
 
-                        edge_indices = torch_geometric.nn.knn_graph(coordinates,k=k,loop=False)
-                        # edge_indicesxy = torch_geometric.nn.knn_graph(feature_tensor[:, [0,1]],k=k,loop=False)
-                        event_graph  = torch_geometric.data.Data(x=torch.column_stack([coordinates,cell_delta_R]),edge_index=edge_indices,y=cell_ids) 
-                        data_list.append(event_graph)
-                        cluster_list.append(cluster_cells_i)
-    
-    return data_list, cluster_list
+    df = pd.DataFrame({
+        'n_clusters' : n_clusters_per_box,
+        'n_cells' : n_cells_per_box,
+        'n_2sigcells' : n_cells2sig_per_box,
+        'n_4sigcells' : n_cells4sig_per_box,
+        'eta' : box_etas,
+        'significance' : box_significance,
+        'area' : box_areas,
+    })
+    return df
 
 
 
@@ -105,26 +105,14 @@ def make_calo_data_list(
 if __name__=="__main__":
 
     box_eta_cut=2.1
-    cell_significance_cut=2
-    norm=False
-    k=3
 
     with open("../../struc_array.npy", "rb") as file:
         inference = np.load(file)
 
-    data_list, topocl_list = make_calo_data_list(inference,
-                                                 box_eta_cut,
-                                                 cell_significance_cut,
-                                                 k)
-
-    with open(f'datasets/data_xyzdeltaR_{len(data_list)}_{box_eta_cut}_{cell_significance_cut}_{k}.pkl', 'wb') as f1:
-        pickle.dump(data_list, f1)
-
-    with open(f'datasets/topocl_data_{len(topocl_list)}.pkl', 'wb') as f2:
-        pickle.dump(topocl_list, f2)
-
-
-
-
-
+    data_df = make_bdt_training(inference,box_eta_cut)
+    print(data_df.columns)
+    print(data_df.shape)
+    print(data_df.head)
+    # data_df.to_parquet("datasets/overall_stats/bdt_n_cluster.parquet")
+    data_df.to_pickle("datasets/overall_stats/bdt_n_cluster.pkl")
 
