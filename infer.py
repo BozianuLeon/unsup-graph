a02b4ae1e2a09387b40c3de86c26e0cd4776ad09
bozianuleon <74475015+BozianuLeon@users.noreply.github.com>
2025-03-07 21:15:29 +0100

    Clean up infer, save only essentials, add 2d plots + jet finding

--- infer.py
+++ infer.py
@@ -89,6 +89,7 @@ if __name__=='__main__':
         "BS"         : args.batch_size,
         "n_clus"     : int(args.num_clusters),
         "n_epochs"   : int(args.epochs),
+        "max_num"    : 10000,
     }
     torch.manual_seed(config["seed"])
 
@@ -122,7 +123,7 @@ if __name__=='__main__':
 
 
     # instantiate numpy structured array to hold inference results
-    dt = np.dtype([('event_no', 'i4'), ('node_features', 'f4', (350, 3)), ('model_output', 'f4', (config["n_clus"], 128)), ('cluster_assign', 'i4', (350,1))])
+    dt = np.dtype([('event_no', 'i4'), ('cell_ID', 'f4', (config["max_num"],1)), ('cluster_assign', 'f4', (config["max_num"],))])
     inference = np.zeros((len(test_loader)*config["BS"]), dtype=dt)  
     model.eval()
     beginning = time.perf_counter()
@@ -131,117 +132,104 @@ if __name__=='__main__':
             print(step)
             data = data.to(config["device"])
             pred, tot_loss, clus_ass = model(data.x,data.edge_index,data.batch)
-            # print("loss",tot_loss.shape,tot_loss)
-            # print("pred",pred.shape)
-            # print("cluster",clus_ass.shape)
             node_features = torch_geometric.utils.unbatch(data.x,data.batch)
-            # print("node",data.x.shape)
-            # print('node 0',node_features[0].shape)
-            # print('node 1',node_features[1].shape)
-            # print('node 2',node_features[2].shape)
+            cell_ids = torch_geometric.utils.unbatch(data.y,data.batch)
+            
 
             # make lists for later padding
-            total_pred, total_clus, total_loss = list(), list(), list()
-            event_nos, feat_mats = list(), list()
+            total_pred_class,total_cell_id,event_nos = [],[],[]
             for batch_idx in range(config["BS"]):
                 # remove from GPU 
                 pred_i = pred[batch_idx].detach().cpu().numpy()
                 clus_i = clus_ass[batch_idx].detach().cpu()
-                # print('\tclus_i',clus_i.shape)
-                # print('\tclus_1',clus_ass[batch_idx+1].detach().cpu().shape)
-                # print('\tclus_2',clus_ass[batch_idx+2].detach().cpu().shape)
+
                 clus_i = clus_i[~np.isnan(clus_i).any(axis=1).bool()] # remove nan values
-                # print('\tclus_i',clus_i.shape)
-                # print('\tclus_1',clus_ass[batch_idx+1].detach().cpu()[~np.isnan(clus_ass[batch_idx+1].detach().cpu()).any(axis=1).bool()].shape)
-                # print('\tclus_2',clus_ass[batch_idx+2].detach().cpu()[~np.isnan(clus_ass[batch_idx+2].detach().cpu()).any(axis=1).bool()].shape)
-                # loss_i = tot_loss[b]
                 x_i = node_features[batch_idx].detach().cpu().numpy()
+                y_i = cell_ids[batch_idx].detach().cpu().numpy()
 
                 # force each node to its most likely cluster, no soft assignment
                 predicted_classes = clus_i.squeeze().argmax(dim=1).numpy()
-                print("\tAlready out of shape here",x_i.shape,pred_i.shape,clus_i.shape,predicted_classes.shape)
                 unique_values, counts = np.unique(predicted_classes, return_counts=True)
                 print(f"\t{len(unique_values)} clusters formed, potential max {config['n_clus']}")
-                for value, count in zip(unique_values, counts):
-                    print(f"\tCluster {value}: {count} occurrences")
-
-                ##########################################################################################################################################
-                fig = plt.figure(figsize=(10, 6))
-                edge_index = torch_geometric.utils.unbatch_edge_index(data.edge_index,data.batch)
-                edg_i = edge_index[batch_idx]
-                unique_values, counts = np.unique(predicted_classes, return_counts=True)
-                ax = fig.add_subplot(121, projection='3d')
-                ax.scatter(x_i[:, 0], x_i[:, 1], x_i[:, 2], s=x_i[:, -1]/3, c='b', marker='o', label='Nodes')
-                for src, dst in edg_i.t().tolist():
-                    x_src, y_src, z_src, *feat = x_i[src]
-                    x_dst, y_dst, z_dst, *feat = x_i[dst]
-                    ax.plot([x_src, x_dst], [y_src, y_dst], [z_src, z_dst], c='r')
-                ax.set(xlabel='X',ylabel='Y',zlabel='Z',title=f'Input Graph with KNN 3 Edges')
-
-                ax2 = fig.add_subplot(122, projection='3d')
-                print('\tFinal here',x_i.shape, edg_i.shape,predicted_classes.shape)
-                cmap = matplotlib.colormaps['Dark2']
-                scatter = ax2.scatter(x_i[:, 0], x_i[:, 1], x_i[:, 2], s=x_i[:, -1]/3, c=predicted_classes, marker='o', cmap=cmap) #Dark2
-                labels = [f"{value}: ({count})" for value,count in zip(unique_values, counts)]
-                # ax2.legend(handles=scatter.legend_elements(num=None)[0],labels=labels,title=f"Classes {len(unique_values)}/{config['n_clus']}",bbox_to_anchor=(1.07, 0.25),loc='lower left')
-                ax2.set(xlabel='X',ylabel='Y',zlabel='Z',title=f'DMoN Output Graph')
-                x_axis = ax2.get_xlim()
-                y_axis = ax2.get_ylim()
-                z_axis = ax2.get_zlim()
-                fig.savefig(f"{save_loc}/plots/results/data_{step*batch_idx+batch_idx}_3d.png", bbox_inches="tight")
-                plt.close()
                 
-                f,a = plt.subplots(1,1,figsize=(10,12))   
-                a.scatter(x_i[:, 3], x_i[:, 4], s=10, c=predicted_classes, cmap='tab20b')
-                a.set(xlabel='eta',ylabel='phi',title=f'DMoN Output Clustered Cells')
-                f.savefig(f"{save_loc}/plots/results/data_{step*batch_idx+batch_idx}_cells_2d.png")
-                plt.close()
+                # ##########################################################################################################################################
+                # for value, count in zip(unique_values, counts):
+                #     print(f"\tCluster {value}: {count} occurrences")
+                # fig = plt.figure(figsize=(10, 6))
+                # edge_index = torch_geometric.utils.unbatch_edge_index(data.edge_index,data.batch)
+                # edg_i = edge_index[batch_idx]
+                # unique_values, counts = np.unique(predicted_classes, return_counts=True)
+                # ax = fig.add_subplot(121, projection='3d')
+                # ax.scatter(x_i[:, 0], x_i[:, 1], x_i[:, 2], s=x_i[:, -1]/3, c='b', marker='o', label='Nodes')
+                # for src, dst in edg_i.t().tolist():
+                #     x_src, y_src, z_src, *feat = x_i[src]
+                #     x_dst, y_dst, z_dst, *feat = x_i[dst]
+                #     ax.plot([x_src, x_dst], [y_src, y_dst], [z_src, z_dst], c='r')
+                # ax.set(xlabel='X',ylabel='Y',zlabel='Z',title=f'Input Graph with KNN 3 Edges')
 
-                fi,ax = plt.subplots(1,1,figsize=(10,12)) 
-                ax.set_prop_cycle(color=plt.cm.tab20b(np.linspace(0, 1, 20)))
-                for cl_idx in range(len(unique_values)):
-                    cluster_i_mask = predicted_classes == cl_idx
-                    cl_cell_i = x_i[cluster_i_mask, :]
-                    cl_eta, cl_phi = weighted_mean(cl_cell_i[:,3], cl_cell_i[:,5]), weighted_circular_mean(cl_cell_i[:,4], cl_cell_i[:,5])
-                    cl_e = np.mean(cl_cell_i[:,5])
-                    ax.plot(cl_eta, cl_phi, markersize=8, marker='o',alpha=.6)
-                ax.set(xlabel='eta',ylabel='phi',title=f'DMoN Output Clusters')
-                fi.savefig(f"{save_loc}/plots/results/data_{step*batch_idx+batch_idx}_cl_2d.png")
-                plt.close()
-
-                # plot individual clusters
-                i_want_cluster = input('Enter cluster number to inspect (type q for exit):')
-                while i_want_cluster!="q":
-                    cluster_i_mask = predicted_classes == int(i_want_cluster)
-                    print(cluster_i_mask)
-                    fig = plt.figure(figsize=(10, 6))
-                    ax1 = fig.add_subplot(111, projection='3d')
-                    scatter = ax1.scatter(x_i[cluster_i_mask, 0], x_i[cluster_i_mask, 1], x_i[cluster_i_mask, 2], s=x_i[cluster_i_mask, -1], marker='o', c="crimson")
-                    ax1.set(xlabel='X',ylabel='Y',zlabel='Z',xlim=x_axis,ylim=y_axis,zlim=z_axis,title=f'DMoN Output Graph Cluster {i_want_cluster}')
-                    plt.show()
-                    i_want_cluster = input('Enter cluster number to inspect (type q for exit):')
-
-                quit()
-                ##########################################################################################################################################
+                # ax2 = fig.add_subplot(122, projection='3d')
+                # print('\tFinal here',x_i.shape, edg_i.shape,predicted_classes.shape)
+                # cmap = matplotlib.colormaps['Dark2']
+                # scatter = ax2.scatter(x_i[:, 0], x_i[:, 1], x_i[:, 2], s=x_i[:, -1]/3, c=predicted_classes, marker='o', cmap=cmap) #Dark2
+                # labels = [f"{value}: ({count})" for value,count in zip(unique_values, counts)]
+                # # ax2.legend(handles=scatter.legend_elements(num=None)[0],labels=labels,title=f"Classes {len(unique_values)}/{config['n_clus']}",bbox_to_anchor=(1.07, 0.25),loc='lower left')
+                # ax2.set(xlabel='X',ylabel='Y',zlabel='Z',title=f'DMoN Output Graph')
+                # x_axis = ax2.get_xlim()
+                # y_axis = ax2.get_ylim()
+                # z_axis = ax2.get_zlim()
+                # fig.savefig(f"{save_loc}/plots/results/data_{step*batch_idx+batch_idx}_3d.png", bbox_inches="tight")
+                # plt.close()
                 
-                total_pred.append(pred_i)
-                total_clus.append(clus_i)
-                # total_loss.append(loss_i)
-                feat_mats.append(x_i)
-                event_nos.append(step*config["BS"]+b)
+                # f,a = plt.subplots(1,1,figsize=(10,12))   
+                # a.scatter(x_i[:, 3], x_i[:, 4], s=10, c=predicted_classes, cmap='tab20b')
+                # a.set(xlabel='eta',ylabel='phi',title=f'DMoN Output Clustered Cells')
+                # f.savefig(f"{save_loc}/plots/results/data_{step*batch_idx+batch_idx}_cells_2d.png")
+                # plt.close()
 
+                # fi,ax = plt.subplots(1,1,figsize=(10,12)) 
+                # ax.set_prop_cycle(color=plt.cm.tab20b(np.linspace(0, 1, 20)))
+                # for cl_idx in range(len(unique_values)):
+                #     cluster_i_mask = predicted_classes == cl_idx
+                #     cl_cell_i = x_i[cluster_i_mask, :]
+                #     cl_eta, cl_phi = weighted_mean(cl_cell_i[:,3], cl_cell_i[:,5]), weighted_circular_mean(cl_cell_i[:,4], cl_cell_i[:,5])
+                #     cl_e = np.mean(cl_cell_i[:,5])
+                #     ax.plot(cl_eta, cl_phi, markersize=8, marker='o',alpha=.6)
+                # ax.set(xlabel='eta',ylabel='phi',title=f'DMoN Output Clusters')
+                # fi.savefig(f"{save_loc}/plots/results/data_{step*batch_idx+batch_idx}_cl_2d.png")
+                # plt.close()
 
+                # # plot individual clusters
+                # i_want_cluster = input('Enter cluster number to inspect (type q for exit):')
+                # while i_want_cluster!="q":
+                #     cluster_i_mask = predicted_classes == int(i_want_cluster)
+                #     print(cluster_i_mask)
+                #     fig = plt.figure(figsize=(10, 6))
+                #     ax1 = fig.add_subplot(111, projection='3d')
+                #     scatter = ax1.scatter(x_i[cluster_i_mask, 0], x_i[cluster_i_mask, 1], x_i[cluster_i_mask, 2], s=x_i[cluster_i_mask, -1], marker='o', c="crimson")
+                #     ax1.set(xlabel='X',ylabel='Y',zlabel='Z',xlim=x_axis,ylim=y_axis,zlim=z_axis,title=f'DMoN Output Graph Cluster {i_want_cluster}')
+                #     plt.show()
+                #     i_want_cluster = input('Enter cluster number to inspect (type q for exit):')
+
+                # quit()
+                # ##########################################################################################################################################
+ 
+                total_pred_class.append(predicted_classes)
+                total_cell_id.append(y_i)
+                event_nos.append(step*config["BS"]+batch_idx)
+
+            # TODO: Need to work out how to save the cluster information here
+            # also including the topocluster information - maybe in metrics?
             dataset_idx = step*config["BS"]
+            print([len(class_array) for class_array in total_pred_class])
+            pred_class_pad = [np.pad(class_array, ((0,config["max_num"]-len(class_array))), 'constant', constant_values=(0)) for class_array in total_pred_class]
+            cell_ids_pad = [np.pad(id_array, ((0,config["max_num"]-len(id_array)),(0,0)), 'constant', constant_values=(0)) for id_array in total_cell_id]
 
-            cl_array   = [np.pad(cl_i, ((0,350-len(cl_i))), 'constant', constant_values=(0)) for cl_i in total_clus]
-            out_array  = [np.pad(out, ((0,config["n_clus"]-len(out)),(0,0)), 'constant', constant_values=(0)) for out in total_pred]
-            feat_array = [np.pad(x, ((0,350-len(x)),(0,0)), 'constant', constant_values=(0)) for x in feat_mats]
-            # t_boxes = [np.pad(trub, ((0,250-len(trub)),(0,0)), 'constant', constant_values=(0)) for trub in tru_boxes]
-
+    
             inference['event_no'][dataset_idx:dataset_idx+config["BS"]] = event_nos
-            inference['node_features'][dataset_idx:dataset_idx+config["BS"]] = feat_array   
-            inference['model_output'][dataset_idx:dataset_idx+config["BS"]] = out_array   
-            inference['cluster_assign'][dataset_idx:dataset_idx+config["BS"]] = cl_array 
+            inference['cell_ID'][dataset_idx:dataset_idx+config["BS"]]  = cell_ids_pad
+            inference['cluster_assign'][dataset_idx:dataset_idx+config["BS"]]  = pred_class_pad
+
+
 
     end = time.perf_counter()      
     print(f"Time taken for entire test set: {(end-beginning)/60:.3f} mins, (or {(end-beginning):.3f}s), average {(end-beginning)/test_len:.4f} per image")
@@ -249,4 +237,4 @@ if __name__=='__main__':
     print('\n\n')
     with open(save_loc+'struc_array.npy', 'wb') as f:
         print('Saving...')
-        np.save(f, Large)
+        np.save(f, inference)
